b39a26c7141eb6e6f06d87151c6f1222ea040b14
Matt Vasil <30942725+basilvasil@users.noreply.github.com>
2021-07-27 16:53:18 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println(testMethod());
+    }
+
+    public static String testMethod() {
+        String s = "This is a method";
+        return s;
+    }
+}
