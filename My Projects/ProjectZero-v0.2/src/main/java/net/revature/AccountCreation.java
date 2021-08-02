493d1372d67433bf94aa93f01d18a13b86c01eea
Matthew <matthew.vasil@revature.net>
2021-08-02 13:00:31 -0400

    Removed the 'Menu' class

--- My Projects/ProjectZero-v0.2/src/main/java/net/revature/AccountCreation.java
+++ My Projects/ProjectZero-v0.2/src/main/java/net/revature/AccountCreation.java
@@ -1,68 +1,63 @@
 package net.revature;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class AccountCreation {
 
+    String firstName;
+    String lastName;
+    String emailAddress;
+    String streetAddress;
+    String city;
+    String state;
+    String zipCode;
 
-
-    public static void creationOfAccount() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter your first name:");
-        String firstName = input.nextLine();
-        System.out.println("Enter your last name:");
-        String lastName = input.nextLine();
-        System.out.println("Enter your street address:");
-        String streetAddress = input.nextLine();
-        System.out.println("Enter your city:");
-        String city = input.nextLine();
-        System.out.println("Enter your state:");
-        String state = input.nextLine();
-        System.out.println("Enter your zip code:");
-        String zipCode = input.nextLine();
-
-        boolean i = true;
-        while (i) {
-            System.out.println("Is the following correct?\n" +
-                    "Name: " + firstName + " " + lastName + "\n" +
-                    "Street Address: " + streetAddress + "\n" +
-                    "City: " + city + "\n" +
-                    "State: " + state + "\n" +
-                    "Zip Code: " + zipCode + "\n" +
-                    "Enter '1' for 'Yes' or '2' for no");
-            int verification = input.nextInt();
-            if (verification == 1) {
-                int accountNumber = 100000000 + new Random().nextInt(900000000);
-                String accountString = Integer.toString(accountNumber);
-                String[] account = new String[7];
-                account[0] = firstName;
-                account[1] = lastName;
-                account[2] = streetAddress;
-                account[3] = city;
-                account[4] = state;
-                account[5] = zipCode;
-                account[6] = accountString;
-
-                System.out.println("Your account number is " + accountString +
-                        "\nPress '1' to return to the main menu" +
-                        "\nPress any other key to close the application");
-
-                int accountFinished = input.nextInt();
-                if (accountFinished == 1) {
-                    Menu.mainMenu();
-                } else {
-                    System.out.println("Goodbye!");
-                    i = false;
-                    System.exit(0);
-                }
-
-            } else if ( verification == 2) {
-                System.out.println("Lets try again\n");
-                creationOfAccount();
-            } else {
-                System.out.println("Invalid entry. Try again");
-            }
-        }
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
     }
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setLastName(String lastName){
+        this.lastName = lastName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setEmailAddress(String emailAddress){
+        this.emailAddress = emailAddress;
+    }
+    public String getEmailAddress(){
+        return emailAddress;
+    }
+
+    public void setStreetAddress(String streetAddress){
+        this.streetAddress = streetAddress;
+    }
+    public String getStreetAddress(){
+        return streetAddress;
+    }
+
+    public void setCity(String city){
+        this.city = city;
+    }
+    public String getCity(){
+        return city;
+    }
+
+    public void setState(String state){
+        this.state = state;
+    }
+    public String getState(){
+        return state;
+    }
+
+    public void setZipCode(String zipCode){
+        this.zipCode = zipCode;
+    }
+    public String getZipCode(){
+        return zipCode;
+    }
+
+
 }
