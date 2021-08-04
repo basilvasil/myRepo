310ecdb38a4c94b276ebd05fbc83d1cf90104f91
Matthew <matthew.vasil@revature.net>
2021-08-03 20:27:20 -0400

    Reverted the changes to the Account Creation Class

--- My Projects/MavenProject0/src/main/java/mainPackage/AccountCreation.java
+++ My Projects/MavenProject0/src/main/java/mainPackage/AccountCreation.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class AccountCreation {
     private static String firstName;
     private static String lastName;
-    private static String  streetAddress;
+    private static String streetAddress;
     private static String city;
     private static String state;
     private static String zipCode;
@@ -27,48 +27,43 @@ public class AccountCreation {
         System.out.println("Enter your zip code:");
         zipCode = input.nextLine();
     }
-    public static void verificationOfAccount(){
+
+    private static void accountFinished(int accountChoice){
+        if (accountChoice != 1) {
+            System.out.println("Goodbye!");
+            System.exit(0);
+        }
+    }
+
+    public static void verificationOfAccount() {
         Scanner input = new Scanner(System.in);
-        boolean i = true;
-        while (i) {
-            System.out.println("Is the following correct?\n" +
-                    "Name: " + firstName + " " + lastName + "\n" +
-                    "Street Address: " + streetAddress + "\n" +
-                    "City: " + city + "\n" +
-                    "State: " + state + "\n" +
-                    "Zip Code: " + zipCode + "\n" +
-                    "Enter '1' for 'Yes' or '2' for no");
+        while (true) {
             int verification = input.nextInt();
-            if (verification == 1) {
-                int accountNumber = 100000000 + new Random().nextInt(900000000);
-                String accountString = Integer.toString(accountNumber);
-                String[] account = new String[7];
-                account[0] = firstName;
-                account[1] = lastName;
-                account[2] = streetAddress;
-                account[3] = city;
-                account[4] = state;
-                account[5] = zipCode;
-                account[6] = accountString;
 
-                System.out.println("Your account number is " + accountString +
-                        "\nPress '1' to return to the main menu" +
-                        "\nPress any other key to close the application");
+            switch (verification) {
+                case 1:
+                    int accountNumber = 100000000 + new Random().nextInt(900000000);
+                    String accountString = Integer.toString(accountNumber);
+                    String[] account = new String[7];
+                    account[0] = firstName;
+                    account[1] = lastName;
+                    account[2] = streetAddress;
+                    account[3] = city;
+                    account[4] = state;
+                    account[5] = zipCode;
+                    account[6] = accountString;
 
-                int accountFinished = input.nextInt();
-                if (accountFinished == 1) {
+                    System.out.println("Your account number is " + accountString +
+                            "\nPress '1' to return to the main menu" +
+                            "\nPress any other key to close the application");
+                    int verificationChoice = input.nextInt();
+                    accountFinished(verificationChoice);
                     break;
-                } else {
-                    System.out.println("Goodbye!");
-                    i = false;
-                    System.exit(0);
-                }
+                case 2:
+                    System.out.println("Lets try again\n");
+                default:
+                    System.out.println("Invalid entry. Try again");
 
-            } else if ( verification == 2) {
-                System.out.println("Lets try again\n");
-                break;
-            } else {
-                System.out.println("Invalid entry. Try again");
             }
         }
     }
